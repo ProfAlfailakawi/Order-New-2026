449d36f9f1b2bc091a8429e3e7b8c774237af081
ProfAlfailakawi <ah_f@hotmail.com>
2026-05-14 01:13:36 +0300

    Delete Order3 directory

--- Order3/src/services/backendService.ts
+++
@@ -1,5 +0,0 @@
-export const OLD_BACKEND_URL = process.env.OLD_BACKEND_URL || "https://order-119610604304.europe-west3.run.app";
-
-export const backendService = {
-  getOldBackendUrl: () => OLD_BACKEND_URL,
-};
