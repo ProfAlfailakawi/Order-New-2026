af409559fe08aeb182fcc7fd35d96c4b50d9a52e
ProfAlfailakawi <ah_f@hotmail.com>
2026-05-14 01:14:10 +0300

    Delete Order directory

--- Order/ping.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-axios.post('http://localhost:3000/api/create-split-payment', {}).then(console.log).catch(err => {
-    console.log(err.response?.status, err.response?.data);
-});
